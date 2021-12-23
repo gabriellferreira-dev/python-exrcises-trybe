8e7874f43b108ee75cf3a5247c9d16e1302be6db
Gabriel Ferreira <glucas709@gmail.com>
2021-12-23 17:12:16 -0300

    feat: function create triangle of asteriscs

--- bloco-32/dia-1/bonus.py
+++ bloco-32/dia-1/bonus.py
@@ -4,4 +4,13 @@ def minValue(list):
     print(min(list))
 
 
-minValue([5, 9, 3, 19, 70, 8, 100, 2, 35, 27])
+# minValue([5, 9, 3, 19, 70, 8, 100, 2, 35, 27])
+
+# Exercício 2 - criar triângulo com n astericos
+
+def createTriangle(n):
+    for i in range(n):
+        print((i + 1) * '*')
+
+
+createTriangle(5)
