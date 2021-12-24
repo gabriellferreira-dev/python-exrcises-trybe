7b7c4ff1b6db921c16f85fd6a8d1e0abeb93d9b4
Gabriel Ferreira <glucas709@gmail.com>
2021-12-24 18:41:35 -0300

    feat: function to calculate percentage of each book category

--- bloco-32/dia-2/index.py
+++ bloco-32/dia-2/index.py
@@ -1,5 +1,6 @@
 import random
-
+import json
+import csv
 # Exercício 1 - imprimir nome na vestical em escada invertida
 
 
@@ -52,5 +53,72 @@ def readWordsFile():
     return words
 
 
-words = readWordsFile()
-scrambledWordGame(words)
+# words = readWordsFile()
+# scrambledWordGame(words)
+
+
+def readJsonFile():
+    with open('books.json', 'r') as file:
+        data = json.load(file)
+
+    return data
+
+
+def groupBooks(list):
+    books = {}
+    totalBooks = 0
+
+    for item in list:
+        if len(item['categories']):
+            for category in item['categories']:
+                if category == '':
+                    break
+
+                totalBooks += 1
+
+                if category in books:
+                    books[category].append(item)
+                else:
+                    books[category] = [item]
+        else:
+            if 'noCategory' in books:
+                books['noCategory'].append(item)
+            else:
+                books['noCategory'] = [item]
+
+    return (books, totalBooks)
+
+
+def booksPercentageByCategory(booksByCategories, totalBooks):
+    keys = booksByCategories.keys()
+    countedBooks = map(lambda key: dict({
+        'categoria': key,
+        'porcentagem': len(
+            booksByCategories[key]) * 100 / totalBooks
+    }), keys
+    )
+
+    return list(countedBooks)
+
+
+def writeCSVFile(filename, data):
+    with open(filename, 'w', newline='') as file:
+        fieldNames = ['categoria', 'porcentagem']
+        writer = csv.DictWriter(
+            file, fieldnames=fieldNames, extrasaction='ignore')
+
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def calcPercentage():
+    data = readJsonFile()
+    booksByCategory, totalBooks = groupBooks(data)
+
+    quantityByCategories = booksPercentageByCategory(
+        booksByCategory, totalBooks)
+
+    writeCSVFile('booksPercentage.csv', quantityByCategories)
+
+
+calcPercentage()
