fb05c02ebada0df9bc7b3fd2e9344e24bf3ed3d2
Gabriel Ferreira <glucas709@gmail.com>
2021-12-24 12:27:31 -0300

    feat: scrambledWordGame

--- bloco-32/dia-2/index.py
+++ bloco-32/dia-2/index.py
@@ -1,4 +1,7 @@
+import random
+
 # Exercício 1 - imprimir nome na vestical em escada invertida
+
 
 def printName(name):
     i = len(name)
@@ -8,4 +11,35 @@ def printName(name):
         i -= 1
 
 
-printName('PEDRO')
+# printName('PEDRO')
+
+# Exercício 2 - Jogo da palavra embaralhada.
+
+def scrambledWordGame(words):
+    randomWord = random.choice(words)
+    wordChars = list(randomWord)
+    random.shuffle(wordChars)
+    scrambled_word = ''.join(wordChars)
+
+    rounds = 3
+
+    while rounds > 0:
+        print(scrambled_word)
+        response = input('Qual a palavra embaralhada acima?\n')
+        print()
+
+        if rounds == 1 and response != randomWord:
+            print('A palavra certa era {}. Você perdeu!'.format(randomWord))
+            print()
+            break
+        elif response != randomWord:
+            print('Palavra errada. Tentativas restantes {}'.format(rounds - 1))
+            print()
+        else:
+            print('Parabéns, palavra correta.')
+            break
+
+        rounds -= 1
+
+
+scrambledWordGame(['Janaina', 'Comer', 'Vacina'])
